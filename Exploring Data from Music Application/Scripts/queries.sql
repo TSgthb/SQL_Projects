0216946bca7953afa6b489d971bb6d3d059c3af9
Tejveer Singh <73650424+TSgthb@users.noreply.github.com>
2025-10-21 20:24:02 +0530

    Enhance queries with documentation

--- Exploring Data from Music Application/Scripts/queries.sql
+++ Exploring Data from Music Application/Scripts/queries.sql
@@ -2,9 +2,7 @@
 ===================================================================================
 Script for analyzing Movies & Shows dataset
 ===================================================================================
-*/
 
-/*
 CREATE DATABASE music_app_data;
 GO
 
@@ -196,4 +194,188 @@ GROUP BY
 ORDER BY max_energy DESC
 GO
 
--- 3. Find the top 5 tracks with the highest energy values.
+USE music_app_data;
+GO
+
+-- 3. List all tracks along with their views and likes where official_video = TRUE.
+
+SELECT
+	track,
+	album,
+	artist,
+	SUM(no_of_views) AS total_views,
+	SUM(likes) AS total_likes
+FROM dbo.view_music_info
+WHERE official_video = 'True'
+GROUP BY
+	track,
+	album,
+	artist
+ORDER BY
+	total_views DESC,
+	total_likes DESC
+GO
+
+-- 4. For each album, calculate the total views of all associated tracks.
+
+SELECT
+	album,
+	artist,
+	SUM(no_of_views) AS total_views
+FROM dbo.view_music_info
+GROUP BY
+	track,
+	album,
+	artist
+ORDER BY
+	total_views DESC
+GO
+
+-- 5. For each album, calculate the total views of all associated tracks.
+
+/*
+WITH views_and_streams AS
+(
+SELECT 
+	track,
+	album,
+	artist,
+	most_played_on,
+	CASE
+	WHEN LOWER(most_played_on) = 'YouTube' THEN MAX(no_of_streams)
+	ELSE 0
+	END AS most_streamed_on_YouTube,
+	CASE
+	WHEN LOWER(most_played_on) = 'Spotify' THEN MAX(no_of_streams)
+	ELSE 0
+	END AS most_streamed_on_Spotify
+--	MAX(no_of_streams) AS total_streams,
+--	MAX(no_of_views) AS total_views
+	FROM dbo.view_music_info
+GROUP BY
+	track,
+	album,
+	artist,
+	most_played_on
+)
+SELECT DISTINCT
+	vas.track,
+	vas.most_streamed_on_Spotify AS Spotify_rank
+FROM views_and_streams AS vas
+WHERE vas.most_streamed_on_Spotify != 0
+ORDER BY Spotify_rank DESC
+GO
+*/
+
+SELECT DISTINCT
+			track,
+			album
+FROM dbo.view_music_info 
+WHERE most_played_on = 'Spotify'
+GO
+
+-- Data Analysis - Category 3
+
+-- 1. Find the top 3 most-viewed tracks for each artist using window functions.
+
+SELECT *
+FROM
+	(
+		SELECT 
+			TRIM(artist) AS clean_artist,
+			TRIM(track) AS clean_track,
+			SUM(no_of_views) AS total_views,
+			ROW_NUMBER() OVER (PARTITION BY TRIM(artist) ORDER BY SUM(no_of_views) DESC) AS ranking_status
+		FROM dbo.view_music_info
+--		WHERE artist = 'Aaron Smith'
+		GROUP BY 
+			artist,
+			track
+	) fsq
+WHERE fsq.ranking_status <= 3
+ORDER BY 
+	fsq.clean_artist,
+	fsq.ranking_status
+GO
+
+-- 2. Write a query to find tracks where the liveness score is above the average.
+
+SELECT *
+FROM
+	(
+		SELECT 
+--			TRIM(artist) AS clean_artist,
+--			TRIM(album) AS clean_album,
+			TRIM(track) AS clean_track,
+			liveness,
+			AVG(liveness) OVER () AS avg_liveness
+		FROM dbo.view_music_info
+--		WHERE artist = 'Aaron Smith'
+		GROUP BY 
+			artist,
+			album,
+			track,
+			liveness
+	) fsq
+WHERE fsq.liveness > fsq.avg_liveness
+ORDER BY 
+	liveness DESC
+GO
+
+-- 3. Use a WITH clause to calculate the difference between the highest and lowest energy values for tracks in each album.
+
+WITH energy_diff_album AS
+(
+SELECT
+	album,
+	MAX(energy) AS highest_energy,
+	MIN(energy) AS lowest_energy,
+	AVG(energy) AS avg_energy,
+	COUNT(*) AS total_songs
+FROM dbo.view_music_info
+WHERE album_type = 'Album'
+GROUP BY 
+	album
+)
+SELECT 
+	*,
+	highest_energy - lowest_energy AS energy_difference
+FROM energy_diff_album
+ORDER BY energy_difference
+
+-- 4. Find tracks where the energy-to-liveness ratio is greater than 1.2.
+
+SELECT 
+	track,
+	album,
+	artist,
+	energy_liveness
+FROM dbo.view_music_info 
+WHERE energy_liveness > 1.2
+GROUP BY
+	track,
+	album,
+	artist,
+	energy_liveness
+
+-- 5. Calculate the cumulative sum of likes for tracks ordered by the number of views, using window functions.
+
+WITH pivot_likes_views AS
+(
+SELECT
+	track,
+	album,
+	MAX(likes) AS no_of_likes,
+	MAX(no_of_views) AS total_views
+FROM dbo.view_music_info
+GROUP BY
+	track,
+	album
+) 
+SELECT
+	plv.track,
+	plv.no_of_likes,
+	SUM(plv.no_of_likes) OVER (ORDER BY plv.total_views DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS rolling_sum_of_likes,
+	plv.total_views
+FROM pivot_likes_views AS plv
+GO
